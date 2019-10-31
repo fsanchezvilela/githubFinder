02eab38d9bff4461f102fa717ec170c774a668b1
Fernando <f.sanchezvilela@gmail.com>
2019-10-31 20:09:42 +0100

    Add: Show Alert, Display Profile And Fetch New Repost with Sort

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Init Github
-const github = new Github()
+const github = new Github;
+//Init UI
+const ui = new UI;
 
 // Search Input
 const searchUser = document.getElementById('searchUser');
@@ -15,13 +17,16 @@ searchUser.addEventListener('keyup', (e) => {
     github.getUser(userText).then(data => {
       if (data.profile.message === 'Not Found') {
         //Show Alert
-
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         //Show Profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     })
   } else {
     //Clear Profile
+    ui.clearProfile();
   }
 
 })
--- github.js
+++ github.js
@@ -1,17 +1,25 @@
 class Github {
   constructor() {
-    this.client_id = '809fd7f056fecee0270f',
-      this.client_secret = '058a4e600990bf2f0750fb249c979d74260becf0'
+    this.client_id = '809fd7f056fecee0270f';
+    this.client_secret = '058a4e600990bf2f0750fb249c979d74260becf0';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
-
+      repos
     }
   }
+
+
 }
